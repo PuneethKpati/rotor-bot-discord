7e76fb35f9762fca2e180c98e5cb9f53c5a88b54
puneeth.kpati <puneeth.kpati@gmail.com>
2020-07-07 22:28:53 +1000

    OOP approach with external objects

--- discord_bot.py
+++ discord_bot.py
@@ -2,12 +2,14 @@
 import discord
 from discord.ext import commands
 
+class Rotor(commands.Bot):
+	def __init__(self, command_prefix):
+		super().__init__(command_prefix=command_prefix)
+
+	async def on_ready(self):
+		print('Rotor ready...')
+
+
 token = open('token', 'r').read().strip()
-
-rotor = commands.Bot(command_prefix='$')
-
-@rotor.event
-async def on_ready():
-	print('Rotor ready...')
-
+rotor = Rotor(command_prefix='$')
 rotor.run(token)
---
+++ meeting.py
@@ -0,0 +1,7 @@
+
+class Meeting:
+	def __init__(self):
+		self._id = 
+		self._name = name
+		self._agenda = None
+		self._date = None
